b1b6ff230412499a277cb59ad734d1e32d37a1df
Sokus <SoQs687@gmail.com>
2021-05-18 13:04:02 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+if [ -d "build" ] then
+    rmdir build
+fi
+mkdir build
 pushd ./build
-g++ ../code/input/*.cpp ../code/logic/*.cpp ../code/objects/*.cpp ../code/output/*_LINUX.cpp ../code/utility/*.cpp -o summoned.output
+g++ ../code/input/*.cpp ../code/logic/*.cpp ../code/objects/*.cpp ../code/output/*_LINUX.cpp ../code/utility/*.cpp ../code/*.cpp -o summoned.output
 popd
